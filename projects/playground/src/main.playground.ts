a88bab73170f33627bbca0f2f8c7bac10f5a6a9b
Worawut <tonmanna@gmail.com>
2018-10-13 22:17:51 +0700

    Remove Text from component

--- projects/playground/src/main.playground.ts
+++ projects/playground/src/main.playground.ts
@@ -1,11 +1,12 @@
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { PlaygroundModule } from 'angular-playground';
+import { MatButtonModule, MatCheckboxModule } from '@angular/material';
 
 PlaygroundModule
   .configure({
     selector: 'app-root',
     overlay: false,
-    modules: []
+    modules: [ MatButtonModule, MatCheckboxModule]
   });
 
 platformBrowserDynamic().bootstrapModule(PlaygroundModule);
